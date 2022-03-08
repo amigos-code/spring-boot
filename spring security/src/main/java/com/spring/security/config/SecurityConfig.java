9d29d126e884f97add7d4b5e5090d7d9a7be73fc
Deepak <javadeep92@gmail.com>
2022-03-08 23:21:43 +0530

    Added custom configuration

--- spring security/src/main/java/com/spring/security/config/SecurityConfig.java
+++ spring security/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,2 +1,36 @@
-package com.spring.security.config;public class SecurityConfig {
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * This class will be used for  customizing the spring security.Earlier we had only added spring security
+ * dependency as part of pom.xml due to that using spring security generated password and username as
+ * user we can access endpoint with Basic Authentication.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("Deepak").password(bCryptPasswordEncoder.encode("Deepak")).authorities("read").build();
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.httpBasic();
+       http.authorizeRequests().anyRequest().authenticated();
+    }
 }
--- spring security/src/main/java/com/spring/security/controller/SpringSecurityController.java
+++ spring security/src/main/java/com/spring/security/controller/SpringSecurityController.java
@@ -1,2 +1,13 @@
-package com.spring.security.controller;public class SpringSecurityController {
+package com.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecurityController {
+
+    @GetMapping("/hello")
+public String get(){
+    return "hello";
+}
 }
